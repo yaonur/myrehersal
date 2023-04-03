0d85e3fb4feb45bc675d85fdd9ce24472988a127
ynr <ya_onur@hotmail.com>
2023-04-03 23:36:18 +0300

    fix input text

--- fr/src/routes/login/+page.svelte
+++ fr/src/routes/login/+page.svelte
@@ -5,9 +5,9 @@
 	<h1>Login</h1>
 	<form action="?/login" class="auth-form" method="POST">
 		<label for=""> Email </label>
-		<input type="text" name="email" />
+		<input type="text" name="email" class="text-black"/>
 		<label for=""> Password </label>
-		<input type="password" name="password" />
+		<input type="password" name="password" class="text-black"/>
 		<button class="btn btn-primary">Login</button>
 	</form>
 </main>
